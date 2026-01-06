a01ab6b10d36a8d582af1d6009112763249d031b
Joran Marcy <joran.marcy@gmail.com>
2026-01-06 18:47:24 +0100

    feat: bool variable driven checkbox

--- Scripts/Resources/bool-variable.gd
+++ Scripts/Resources/bool-variable.gd
@@ -1,10 +1,16 @@
 extends Resource
 class_name BoolVariable
 
+signal value_changed(new_value: bool)
+
 @export var initial_value: bool = false:
 	set(new_val):
 		initial_value = new_val
 		value = new_val # Automatically syncs 'value' when 'initial_value' is loaded
 		print("Resource loaded/updated initial_value to: " + str(initial_value))
 
-var value: bool
+var value: bool:
+	set(new_val):
+		if value != new_val:
+			value = new_val
+			value_changed.emit(value)
---
+++ Scripts/bool-driven-checkbox.gd
@@ -0,0 +1,18 @@
+extends CheckBox
+
+@export var bool_var: BoolVariable
+
+func _ready():
+	# To check the box
+	button_pressed = bool_var.value
+	toggled.connect(_on_toggled)
+	bool_var.value_changed.connect(_on_value_changed)
+
+func _on_toggled(pressed: bool):	
+	print("Checkbox toggled to: " + str(pressed))
+	bool_var.value = pressed
+
+func _on_value_changed(new_value: bool):
+	print("BoolVariable changed to: " + str(new_value))
+	button_pressed = new_value	
+	
