ca271d70cc8ec8e723d894a016a83ffaefcc8b34
Joran Marcy <joran.marcy@gmail.com>
2026-01-09 18:57:50 +0100

    feat: ibool variable

--- Scripts/Resources/bool-variable.gd
+++ Scripts/Resources/bool-variable.gd
@@ -6,11 +6,16 @@ signal value_changed(new_value: bool)
 @export var initial_value: bool = false:
 	set(new_val):
 		initial_value = new_val
-		value = new_val # Automatically syncs 'value' when 'initial_value' is loaded
-		print("Resource loaded/updated initial_value to: " + str(initial_value))
+		_value = new_val
+		print("Resource loaded initial_value:", initial_value)
+
+var _value: bool = false
 
 var value: bool:
+	get:
+		return _value
 	set(new_val):
-		if value != new_val:
-			value = new_val
-			value_changed.emit(value)
+		if _value != new_val:
+			_value = new_val
+			value_changed.emit(_value)
+			print("Runtime value changed to:", _value)
--- Scripts/tests/use_bool_variable.gd
+++ Scripts/tests/use_bool_variable.gd
@@ -6,7 +6,7 @@ extends Node
 func _ready() -> void:
 	bool_variable.value_changed.connect(_on_bool_value_changed)
 	print("The bool variable value: " + str(bool_variable.value))
-	bool_variable.value = false
+	#bool_variable.value = false
 	# Change the value after a delay
 	#await get_tree().create_timer(2.0).timeout
 	#bool_variable.value = true
