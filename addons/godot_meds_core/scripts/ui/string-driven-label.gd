8b743c0a2cd25a7fc0151a12227738a8982e40d1
Joran Marcy <joran.marcy@gmail.com>
2026-03-28 12:30:36 +0100

    feat: suffix and prefix for string driven labels

--- addons/godot_meds_core/scripts/ui/string-driven-label.gd
+++ addons/godot_meds_core/scripts/ui/string-driven-label.gd
@@ -1,9 +1,14 @@
 extends Label
 @export var string_variable: StringVariable
+@export var prefix: String = ""
+@export var suffix: String = ""
 
 func _ready():
-	self.text = string_variable.value
+	_update_text(string_variable.value)
 	string_variable.value_changed.connect(_on_value_changed)
 
 func _on_value_changed(new_value: String):
-	self.text = new_value
+	_update_text(new_value)
+
+func _update_text(value: String) -> void:
+	self.text = prefix + value + suffix
