c1d56f12e146d395ff30d0c901b6f2fc2b730786
Joran Marcy <joran.marcy@gmail.com>
2026-03-31 20:43:51 +0200

    feat: migrate float-driven-slider to numeric-driven-slider and deprecate old implementation

--- addons/godot_meds_core/scripts/ui/float-driven-slider.gd
+++ addons/godot_meds_core/scripts/ui/float-driven-slider.gd
@@ -1,33 +1,23 @@
-extends Slider
+extends "res://addons/godot_meds_core/scripts/ui/numeric-driven-slider.gd"
 
-@export var float_variable: FloatVariable
+static var _deprecation_reported: bool = false
 
-var _default_min_value: float
-var _default_max_value: float
+@export var float_variable: FloatVariable:
+	set(new_value):
+		float_variable = new_value
+		variable = new_value
 
-func _ready():	
-	_default_min_value = min_value
-	_default_max_value = max_value
-	_sync_slider_range()
-	self.value = float_variable.value
-	float_variable.value_changed.connect(_on_variable_value_changed)
-	float_variable.range_changed.connect(_on_variable_range_changed)
-	value_changed.connect(_on_slider_value_changed)
+func _ready() -> void:
+	if variable == null and float_variable != null:
+		variable = float_variable
+	_report_deprecation_once()
+	super._ready()
 
-func _on_slider_value_changed(new_value: float):	
-	float_variable.set_value(new_value, self)
+func _report_deprecation_once() -> void:
+	if _deprecation_reported:
+		return
+	_deprecation_reported = true
+	var message := "[DEPRECATED] float-driven-slider.gd is deprecated. Use numeric-driven-slider.gd instead."
+	push_warning(message)
+	printerr(message)
 
-func _on_variable_value_changed(new_value: float):	
-	self.value = new_value
-
-func _on_variable_range_changed(clamp_enabled: bool, new_min_value: float, new_max_value: float) -> void:
-	_sync_slider_range(clamp_enabled, new_min_value, new_max_value)
-
-func _sync_slider_range(clamp_enabled: bool = float_variable.clamp_value, new_min_value: float = float_variable.min_value, new_max_value: float = float_variable.max_value) -> void:
-	if clamp_enabled:
-		min_value = new_min_value
-		max_value = new_max_value
-	else:
-		min_value = _default_min_value
-		max_value = _default_max_value
-	
